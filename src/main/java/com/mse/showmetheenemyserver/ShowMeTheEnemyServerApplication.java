77cd4ece77b562da516b28b0e7770596ea4dea8b
heygwangjin <heygwangjin@gmail.com>
2022-03-19 20:33:43 +0900

    first commit

---
+++ src/main/java/com/mse/showmetheenemyserver/ShowMeTheEnemyServerApplication.java
@@ -0,0 +1,13 @@
+package com.mse.showmetheenemyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMeTheEnemyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowMeTheEnemyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mse/showmetheenemyserver/ShowMeTheEnemyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mse.showmetheenemyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowMeTheEnemyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
